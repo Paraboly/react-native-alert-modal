0ad49cc4862a978f09f0b5965263c7ac95ac9a10
FreakyCoder <kurayogun@gmail.com>
2019-06-25 12:19:59 +0300

    ClassicOnlyText is completed

--- example/lib/src/components/classicOnlyText/ClassicOnlyText.js
+++ example/lib/src/components/classicOnlyText/ClassicOnlyText.js
@@ -2,26 +2,30 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Text, View, Dimensions } from "react-native";
 import RoundedTitle from "../shared/RoundedTitle/RoundedTitle";
+import styles, { textStyle } from "./ClassicOnlyText.style";
 
 const { width } = Dimensions.get("window");
 
 const ClassicOnlyText = props => {
-  const { title, titleColor, titleTextColor } = props;
+  const {
+    text,
+    title,
+    textColor,
+    titleColor,
+    fontFamily,
+    titleTextColor
+  } = props;
   return (
-    <View style={{ justifyContent: "center", alignItems: "center" }}>
+    <View style={styles.container}>
       <RoundedTitle
         title={title}
         titleColor={titleColor}
         titleWidth={width * 0.9}
         titleTextColor={titleTextColor}
       />
-      <View style={{ margin: 16, alignItems: "center", width: width * 0.8 }}>
-        <Text
-          numberOfLines={3}
-          style={{ color: "#828b9a", fontSize: 16, textAlign: "center" }}
-        >
-          2432. Caddesi yürüyüş sebebiyle 9.00 - 17.00 saatleri arasında kapalı
-          olacaktır.
+      <View style={styles.textContainer}>
+        <Text numberOfLines={3} style={textStyle(textColor, fontFamily)}>
+          {text}
         </Text>
       </View>
     </View>
@@ -29,10 +33,15 @@ const ClassicOnlyText = props => {
 };
 
 ClassicOnlyText.propTypes = {
-  title: PropTypes.string
+  text: PropTypes.string,
+  title: PropTypes.string,
+  textColor: PropTypes.string
 };
 
 ClassicOnlyText.defaultProps = {
+  text:
+    "2432. Caddesi yürüyüş sebebiyle 9.00 - 17.00 saatleri arasında kapalı olacaktır.",
+  textColor: "#828b9a",
   title: "Memleket Cad. Duyurusu"
 };
 
